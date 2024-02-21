0af9f9aa07e68ab73179d133cf1d6b0bee071a2a
Marcos Lima <114774379+Marcos-SL@users.noreply.github.com>
2024-02-21 12:38:51 -0300

    Update ClienteController.py

--- Controllers/ClienteController.py
+++ Controllers/ClienteController.py
@@ -2,7 +2,7 @@ import services.database as db;
 
 def Incluir(cliente):
     count = db.cursor.execute("""
-    INSERT INTO Cliente (cliNome, cliIdade, cliProfissao, cliSenha) 
-    VALUES (?,?,?,?)""",
-    cliente.nome, cliente.idade, cliente.profissao, cliente.senha).rowcount
+    INSERT INTO Agenda (nome, corte, telefone, horario, data) 
+    VALUES (?,?,?,?,?)""",
+    cliente.nome, cliente.corte, cliente.telefone, cliente.horario, cliente.data).rowcount
     db.cnxn.commit()
